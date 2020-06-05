3a39b73f290bcd2b73af89a2ba719830a04ede40
Katerina Hanson <katerina.hanson@gmail.com>
2020-06-04 18:38:58 -0700

    Initial Commit

---
+++ app/src/main/java/com/katerinah/coderswag/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.katerinah.coderswag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
