5078b2f1407e0899d4d6541a3b281fd1072157de
Katerina Hanson <katerina.hanson@gmail.com>
2020-06-07 17:35:32 -0700

    add click listener

--- app/src/main/java/com/katerinah/coderswag/Controllers/MainActivity.kt
+++ app/src/main/java/com/katerinah/coderswag/Controllers/MainActivity.kt
@@ -2,6 +2,9 @@ package com.katerinah.coderswag.Controllers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import com.katerinah.coderswag.Adapters.CategoryAdapter
 import com.katerinah.coderswag.Models.Category
 import com.katerinah.coderswag.R
@@ -16,10 +19,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this,
-            DataService.categories
-        )
-
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        categoryListView.setOnItemClickListener {parent: AdapterView<*>?, view: View?, i: Int, l ->
+            val category = DataService.categories[i]
+            Toast.makeText(this, "You clikced on the ${category.title} cell", Toast.LENGTH_LONG).show()
+        }
     }
 }
